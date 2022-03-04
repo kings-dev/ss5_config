5220434939523e5c16d00fec5f4b9b81ad3cb3eb
king <54162645+kings-dev@users.noreply.github.com>
2022-03-04 19:19:43 +0800

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,4 +1,4 @@
-运行环境: 华为云服务器( ECS: Centos 7 ) 网络 ( EIP : IPV4 );
+运行环境: 华为云服务器( ECS: Centos 7 ) 网络 ( EIP : IPv4 & IPv6 );
 Centos 7 系统操作bash shell：复制0.sh里面代码到 bash shell 窗口回车即可（下一步根据指令提示安装等操作回车）;
 命令 sh 0.sh (会下载文件到/root/1.sh);
 命令 sh 1.sh (再次运行脚本安装等).
