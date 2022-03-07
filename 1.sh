5bc1a61216697a5a1085c142cbe128a4fbe2a044
king <54162645+kings-dev@users.noreply.github.com>
2022-03-07 18:04:47 +0800

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -47,8 +47,10 @@ ip=`curl -s ifconfig.me`
             yum -y install pam-devel openldap-devel openssl-devel gcc pam-devel wget net-tools > /dev/null
             wget -nc http://downloads.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz > /dev/null
             tar -xzvf ss5-3.8.9-8.tar.gz > /dev/null 2>&1
-            cd /root/ss5-3.8.9/;./configure > /dev/null 2>&1
-            make > /dev/null 2>&1;make install > /dev/null 2>&1
+            cd /root/ss5-3.8.9/
+            ./configure > /dev/null 2>&1
+            make > /dev/null 2>&1
+            make install > /dev/null 2>&1
             confFile=/etc/opt/ss5/ss5.conf
             sed -i '87c auth    0.0.0.0/0       -               -' $confFile
             sed -i '203c permit -        0.0.0.0/0       -       0.0.0.0/0       -       -       -       -       -' $confFile
