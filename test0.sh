03fc1be487ac0ab0212e491080eedd4471061a67
king <54162645+kings-dev@users.noreply.github.com>
2022-03-08 00:17:53 +0800

    Update test0.sh

--- test0.sh
+++ test0.sh
@@ -4,11 +4,11 @@ cat << "EOF" > /root/test0.sh
 clear
 ping -c 2 raw.githubusercontent.com > /dev/null
 if [ $? -eq 0 ];then
-    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh > /dev/null
+    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh
     cd ~;sh test1.sh
 else
     echo `curl https://ipaddress.com/website/raw.githubusercontent.com`|grep -Po "[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]"|awk '{print $0" raw.githubusercontent.com"}' >> /etc/hosts
-    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh > /dev/null
+    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh
     cd ~;sh test1.sh
 fi
 exit 0
