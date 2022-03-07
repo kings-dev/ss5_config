f8c464f856dcf4a070bb0250f752e9de239ca3d4
king <54162645+kings-dev@users.noreply.github.com>
2022-03-07 23:11:19 +0800

    Create test0.sh

---
+++ test0.sh
@@ -0,0 +1,16 @@
+cat << "EOF" > /root/test0.sh
+#!/bin/bash
+#!/bin/sh
+clear
+ping -c 2 raw.githubusercontent.com > /dev/null
+if [ $? -eq 0 ];then
+    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/1.sh -O /root/test.sh > /dev/null
+    cd ~;sh 1.sh
+else
+    echo `curl https://ipaddress.com/website/raw.githubusercontent.com`|grep -Po "[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]"|awk '{print $0" raw.githubusercontent.com"}' >> /etc/hosts
+    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/1.sh -O /root/test1.sh > /dev/null
+    cd ~;sh test1.sh
+fi
+exit 0
+EOF
+cd ~ && sh test0.sh
