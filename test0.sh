8508477909a0fb9aebeb0670a5385f34a417e23b
king <54162645+kings-dev@users.noreply.github.com>
2022-03-11 01:48:31 +0800

    Update test0.sh

--- test0.sh
+++ test0.sh
@@ -1,12 +1,16 @@
 cat << "EOF" > /root/test0.sh
 #!/bin/bash
 #!/bin/sh
-yum -y install wget  > /dev/null && ping -c 2 raw.githubusercontent.com > /dev/null
-if [[ $? -eq 0 ]];then
-    wget -q --no-check-certificate https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh && cd ~ && sh test1.sh
-else
+yum -y install wget > /dev/null
+ping -c 2 raw.githubusercontent.com|grep "127.0.0.1" > /dev/null
+if [ $? -eq 0 ];then
     echo `curl https://ipaddress.com/website/raw.githubusercontent.com`|grep -Po "[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]"|awk '{print $0" raw.githubusercontent.com"}' >> /etc/hosts
-    wget -q --no-check-certificate https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh && cd ~ && sh test1.sh
+    /etc/init.d/network restart  > /dev/null
+    wget -q --no-check-certificate https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh > /dev/null && cd ~ && sh test1.sh
+    echo "ping no ok"
+else
+    wget -q --no-check-certificate https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh > /dev/null && cd ~ && sh test1.sh
+    echo "ping is ok"
 fi
 exit 0
 EOF
