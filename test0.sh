5d5fb784d598c8d88850fc033a16b9ee9e7b5706
king <54162645+kings-dev@users.noreply.github.com>
2022-03-10 23:31:53 +0800

    Update test0.sh

--- test0.sh
+++ test0.sh
@@ -2,18 +2,13 @@ cat << "EOF" > /root/test0.sh
 #!/bin/bash
 #!/bin/sh
 clear
-yum -y install wget  > /dev/null
-ping -c 2 raw.githubusercontent.com > /dev/null
+yum -y install wget  > /dev/null;ping -c 2 raw.githubusercontent.com > /dev/null
 if [ $? -eq 0 ];then
-    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh
-    cd ~
-    sh test1.sh
+    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh && cd ~ && sh test1.sh
 else
     echo `curl https://ipaddress.com/website/raw.githubusercontent.com`|grep -Po "[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]"|awk '{print $0" raw.githubusercontent.com"}' >> /etc/hosts
-    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh
-    cd ~
-    sh test1.sh
+    wget -q --no-check-certificate --content-disposition https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh && cd ~ && sh test1.sh
 fi
 exit 0
 EOF
-cd ~&&sh test0.sh
+cd ~ && sh test0.sh
