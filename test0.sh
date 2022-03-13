e22035f7b6034688b5877a0fb7b49a0505a2b951
king <54162645+kings-dev@users.noreply.github.com>
2022-03-13 19:33:03 +0800

    Update test0.sh

--- test0.sh
+++ test0.sh
@@ -2,16 +2,7 @@ cat << "EOF" > /root/test0.sh
 #!/bin/bash
 #!/bin/sh
 yum -y install wget > /dev/null
-ping -c 2 raw.githubusercontent.com|grep "127.0.0.1" > /dev/null
-if [ $? -eq 0 ];then
-    echo `curl https://ipaddress.com/website/raw.githubusercontent.com`|grep -Po "[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]\.[0-9][0-9][0-9]"|awk '{print $0" raw.githubusercontent.com"}' >> /etc/hosts
-    /etc/init.d/network restart  > /dev/null
-    wget -q --no-check-certificate --no-certificate-check https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh > /dev/null && cd ~ && sh test1.sh
-    echo "ping no ok"
-else
-    wget -q -no-check-certificate --no-certificate-check https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh > /dev/null && cd ~ && sh test1.sh
-    echo "ping is ok"
-fi
+wget -q --no-check-certificate https://gh.api.99988866.xyz/https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh
 exit 0
 EOF
 cd ~ && sh test0.sh
