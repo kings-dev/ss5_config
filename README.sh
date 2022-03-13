23ce172369ed30580dd36462c09b3da98b202572
king <54162645+kings-dev@users.noreply.github.com>
2022-03-14 00:13:50 +0800

    Update README.sh

--- README.sh
+++ README.sh
@@ -2,7 +2,8 @@
 Centos 7 系统操作bash shell：复制0.sh里面代码到 bash shell 窗口回车即可(下一步根据指令提示安装等操作回车);
 命令 sh huawei_install.sh (再次运行脚本安装等).
 
---------------------------------
+Download: huawei_install.sh
+
 1.code:
 
 wget -q -N --no-check-certificate http://raw.fastgit.org/kings-dev/ss5_config/main/huawei_install.sh;bash huawei_install.sh
