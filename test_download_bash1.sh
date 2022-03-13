e7223d5893290eb8b199618194121d527301d7a8
king <54162645+kings-dev@users.noreply.github.com>
2022-03-13 23:46:04 +0800

    Update and rename test_download_bash.sh to test_download_bash1.sh

--- test_download_bash1.sh
+++ test_download_bash1.sh
@@ -1,8 +1,8 @@
-cat << "EOF" > /root/test_download_bash.sh
+cat << "EOF" > /root/test_download_bash1.sh
 #!/bin/bash
 #!/bin/sh
 yum -y install wget > /dev/null
 wget -q -N --no-check-certificate http://raw.fastgit.org/kings-dev/ss5_config/main/test_install.sh -O /root/test_install.sh && bash test_install.sh
 exit 0
 EOF
-cd ~ && sh test_download_bash.sh
+cd ~ && sh test_download_bash1.sh
