5acccbff632fe4f560a9ff09b8f18ac100a83a79
king <54162645+kings-dev@users.noreply.github.com>
2022-03-13 22:46:14 +0800

    Update test_download_bash.sh

--- test_download_bash.sh
+++ test_download_bash.sh
@@ -2,7 +2,7 @@ cat << "EOF" > /root/test_download_bash.sh
 #!/bin/bash
 #!/bin/sh
 yum -y install wget > /dev/null
-wget -q --no-check-certificate http://raw.fastgit.org/kings-dev/ss5_config/main/test_install.sh -O /root/test_install.sh && bash test_install.sh
+wget -q -N --no-check-certificate http://raw.fastgit.org/kings-dev/ss5_config/main/test_install.sh -O /root/test_install.sh && bash test_install.sh
 exit 0
 EOF
 cd ~ && sh test_download_bash.sh
