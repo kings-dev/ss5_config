4e40d421a10901b88e970a70ebc341a9ced24908
king <54162645+kings-dev@users.noreply.github.com>
2022-03-13 19:35:14 +0800

    Update test0.sh

--- test0.sh
+++ test0.sh
@@ -2,7 +2,7 @@ cat << "EOF" > /root/test0.sh
 #!/bin/bash
 #!/bin/sh
 yum -y install wget > /dev/null
-wget -q --no-check-certificate https://gh.api.99988866.xyz/https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh
+wget -q --no-check-certificate https://gh.api.99988866.xyz/https://raw.githubusercontent.com/kings-dev/ss5_config/main/test1.sh -O /root/test1.sh && bash test1.sh
 exit 0
 EOF
 cd ~ && sh test0.sh
