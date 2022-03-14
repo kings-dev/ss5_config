8057e1441353e963bdacc7012ac44f84cd3162aa
king <54162645+kings-dev@users.noreply.github.com>
2022-03-14 17:20:07 +0000

    Update test_install.sh

--- test_install.sh
+++ test_install.sh
@@ -46,17 +46,17 @@ ip=`curl -s ifconfig.me`
         case $ok in
         [yY][eE][sS]|[yY])
             cd ~
-            echo "安装中 ...................................................................................."
-            echo "Installing ................................................................................"
+            echo "安装中 ..................................................................................."
+            echo "Installing ..............................................................................."
             yum -y install yum-utils pam-devel openldap-devel openssl-devel gcc pam-devel wget net-tools >/dev/null 2>&1
             yum clean all >/dev/null 2>&1;yum-complete-transaction >/dev/null 2>&1
             wget -nc http://downloads.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz >/dev/null 2>&1
             tar -xzvf ss5-3.8.9-8.tar.gz >/dev/null 2>&1
             cd /root/ss5-3.8.9/
-            echo "解压 ......... OK"
+            echo "解压 ........ OK"
             echo "unzip ....... OK"
-            echo "配置 ......... 编译 ...... 稍等"
-            echo "configure ... Compile ... Wait"
+            echo "配置 ........ 编译 ...... 稍等 ..."
+            echo "configure ... Compile ... Wait ..."
             ./configure >/dev/null 2>&1;make >/dev/null 2>&1;make install >/dev/null 2>&1
             echo "安装 ........ OK"
             echo "Install ..... OK"
