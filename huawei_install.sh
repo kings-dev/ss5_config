120489f4654e16676cbd3bb7c8a1336df2d0164f
king <54162645+kings-dev@users.noreply.github.com>
2022-03-15 18:20:42 +0000

    Update huawei_install.sh

--- huawei_install.sh
+++ huawei_install.sh
@@ -45,19 +45,19 @@ ip=`curl -s ifconfig.me`
         >> " ok
         case $ok in
         [yY][eE][sS]|[yY])
-            echo "Yum 下载依赖包环境进行安装中 ....... 耐心等待 ............... [  OK  ]"
+            echo "Yum 下载依赖包环境进行安装中 ................................................. 耐心等待 ..................... [  OK  ]"
             echo "Yum is downloading the dependency package environment for installation ....... Wait patiently ............... [  OK  ]"
             yum -y install yum-utils wget net-tools gcc gcc-c++ automake autoconf libtool make pam-devel openldap-devel cyrus-sasl-devel openssl-devel | xargs -L 19 | xargs -I@ echo -ne "..Yum..====>==>==>==>==>==>==>==>==>==>==>==>==>==>>>>>>>>>[  OK  ]\n"
             yum update -y nss curl libcurl | xargs -L 8 | xargs -I@ echo -ne "..Update..====>==>==>==>==>==>==>==>==>==>==>==>==>==>>>>>>[  OK  ]\n"
             #yum clean all >/dev/null 2>&1;yum-complete-transaction >/dev/null 2>&1
-            echo "正在下载     'ss5-3.8.9-8.tar.gz' .... 耐心等待 ........... [  OK  ]"
+            echo "正在下载    'ss5-3.8.9-8.tar.gz' .... 耐心等待 ........... [  OK  ]"
             echo "Downloading 'ss5-3.8.9-8.tar.gz' .... Wait patiently ..... [  OK  ]"
             wget -nc http://downloads.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz >/dev/null 2>&1
             lj=`pwd`
             tar -xzvf ss5-3.8.9-8.tar.gz | xargs -L 13 | xargs -I@ echo -ne "..Tar -xzvf *.gz..====>==>==>==>==>==>==>==>==>==>==>>>>>>>[  OK  ]\n"
             cd /$lj/ss5-3.8.9/
             ./configure >/dev/null 2>&1;make >/dev/null 2>&1;make install >/dev/null 2>&1
-            echo "===.... 配置 ........ 编译 ....... 安装 ................... [  OK  ]"
+            echo "===.... 配置 ........ 编译 ....... 安装 .................. [  OK  ]"
             echo "===.... Configure ... Make ....... Install ............... [  OK  ]"
             chmod 777 /etc/rc.d/init.d/ss5
             chmod a+x /etc/rc.d/init.d/ss5
