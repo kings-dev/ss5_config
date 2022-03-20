ebbb1089d5cc40a17369116d45bcd0d731e41cce
king <54162645+kings-dev@users.noreply.github.com>
2022-03-20 18:02:17 +0000

    Update test_install.sh

--- test_install.sh
+++ test_install.sh
@@ -85,15 +85,15 @@ EOF
             bash par.sh & yum -y install yum-utils wget net-tools gcc gcc-c++ automake autoconf libtool make pam-devel openldap-devel cyrus-sasl-devel openssl-devel >/dev/null 2>&1
             bash par.sh & yum update -y nss curl libcurl >/dev/null 2>&1
             #yum clean all >/dev/null 2>&1;yum-complete-transaction >/dev/null 2>&1
-            echo "正在下载    'ss5-3.8.9-8.tar.gz' .... 耐心等待 ........... [  OK  ]"
-            echo "Downloading 'ss5-3.8.9-8.tar.gz' .... Wait patiently ..... [  OK  ]"
+            echo -e "\r\n                   正在下载    'ss5-3.8.9-8.tar.gz' .... 耐心等待 ........... [  OK  ]"
+            echo -e "\r\n                   Downloading 'ss5-3.8.9-8.tar.gz' .... Wait patiently ..... [  OK  ]"
             wget -nc http://downloads.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz >/dev/null 2>&1
             lj=`pwd`
-            tar -xzvf ss5-3.8.9-8.tar.gz | xargs -L 13 | xargs -I@ echo -ne "                   ..Tar -xzvf *.gz..====>==>==>==>==>==>==>==>==>==>==>>>>>>>[  OK  ]\n\r"
+            tar -xzvf ss5-3.8.9-8.tar.gz | xargs -L 13 | xargs -I@ echo -ne "\r\n                   ..Tar -xzvf *.gz..====>==>==>==>==>==>==>==>==>==>==>>>>>>>[  OK  ]\n\r"
             cd /$lj/ss5-3.8.9/
             ./configure >/dev/null 2>&1;make >/dev/null 2>&1;make install >/dev/null 2>&1
-            echo -e "                   ===.... 配置 ........ 编译 ....... 安装 .................. [  OK  ]\n\r"
-            echo -e "                   ===.... Configure ... Make ....... Install ............... [  OK  ]\n\r"
+            echo -e "\r\n                   ===.... 配置 ........ 编译 ....... 安装 .................. [  OK  ]\n\r"
+            echo -e "\r\n                   ===.... Configure ... Make ....... Install ............... [  OK  ]\n\r"
             chmod 777 /etc/rc.d/init.d/ss5
             chmod a+x /etc/rc.d/init.d/ss5
             confFile=/etc/opt/ss5/ss5.conf
