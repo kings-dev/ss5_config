7836a7cc94d19273c78ffc03c9d82c38ea168e1d
king <54162645+kings-dev@users.noreply.github.com>
2022-03-22 16:55:58 +0800

    Update test_install.sh

--- test_install.sh
+++ test_install.sh
@@ -51,8 +51,8 @@ ip=`curl -s ifconfig.me`
 cat << "EOF" > ./par.sh
 #!/bin/bash
 echo -e "\n\n"
-ps1=`ps|grep "yum" >/dev/null 2&>1`
-while [ "$ps1" == 0 ];
+ps|grep "yum" >/dev/null 2&>1
+while [ $? == 0 ];
 do
 sleep 0.1 && echo -ne "\r\\  Activity: \\       1%  . "&&
 sleep 0.1 && echo -ne "\r|   Activity: |        4%  .. "&&
@@ -70,8 +70,8 @@ sleep 0.1 && echo -ne "\r\\  :actIve: \\       84%  ............. "&&
 sleep 0.1 && echo -ne "\r|   :acTive: |        90%  .............. "&&
 sleep 0.1 && echo -ne "\r/   :aCtive: /        96%  ............... "&&
 sleep 0.1 && echo -ne "\r-   :Active: -       100%  ................ ";
-ps2=`ps|grep "yum" >/dev/null 2&>1`
-if [ "$ps2" != 0 ];then
+ps|grep "yum" >/dev/null 2&>1
+if [ $? = 0 ];then
 break
 exit
 fi
